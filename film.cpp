8c8062ddc422c1ac8e6a2f597f03e9b0da8de4f8
afshinsh <afshinsharifian@gmail.com>
2019-05-19 12:15:40 +0430

    add

---
+++ film.cpp
@@ -0,0 +1,60 @@
+#include "film.h"
+
+film::film(string _name, string _year, string _price, string _length
+, string _summary , string _director, int _ID_counter_film, customer* _publisher)
+ : name(_name), year(_year), price(_price), length(_length), summary(_summary)
+ , director(_director), ID(_ID_counter_film), publisher(_publisher) {}
+
+ void film::set_owner(customer* new_custormer)
+{
+  owners.push_back(new_custormer);
+}
+
+double film::give_avrage_rate()
+{
+  double sum = 0;
+  for(int i = 0;i < owners.size();i++)
+    sum += owners[i]->get_rate(ID);
+  if(owners.size() == 0)
+    return 0;
+  return sum / owners.size();
+}
+
+void film::set_comment(int film_id, string content)
+{
+  comment* new_comment = new comment(film_id, content, ID_counter_comment);
+  comments.push_back(new_comment);
+  ID_counter_comment++;
+}
+
+void film::reply_cm(int comment_id, string content)
+{
+  for(int i = 0;i < comments.size();i++)
+    if(comments[i]->get_ID() == comment_id)
+    {
+      comments[i]->set_reply(content);
+      return;
+    }
+  throw NotFound();
+}
+
+void film::delete_comment(int comment_id)
+{
+  for(auto i = comments.begin();i != comments.end();i++)
+    if((*i)->get_ID() == comment_id)
+    {
+      delete *i;
+      comments.erase(i);
+      return;
+    }
+  throw NotFound();
+}
+
+void film::show_comment()
+{
+  for(int i = 0;i < comments.size();i++)
+  {
+    cout<<i + 1<<". "<<comments[i]->get_content()<<endl;
+    comments[i]->show_replies(i + 1);
+  }
+}
