d49a33f1cfe247c8650d81492d67b61d2a00209b
afshinsh <afshinsharifian@gmail.com>
2019-05-19 12:15:58 +0430

    add

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "manager.h"
+
+
+using namespace std;
+
+
+int main()
+{
+  string command;
+  manager Manager = manager();
+  while(getline(cin, command))
+  {
+    try {
+      Manager.set_command(command);
+      Manager.process_users();
+    } catch(PermissionDenied ex) {
+      ex.what();
+    } catch(NotFound ex) {
+      ex.what();
+    } catch(BadRequest ex) {
+      ex.what();
+    }
+    Manager.reset();
+  }
+}
