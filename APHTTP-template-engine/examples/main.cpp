74208b92465bb5cf0302dca382a50fbb037c0798
afshinsh <afshinsharifian@gmail.com>
2019-05-27 20:46:46 +0430

    change for adapt with server

--- APHTTP-template-engine/examples/main.cpp
+++ APHTTP-template-engine/examples/main.cpp
@@ -15,8 +15,8 @@ int main(int argc, char **argv) {
     server.setNotFoundErrPage("static/404.html");
     server.get("/login", new ShowPage("static/logincss.html"));
     server.post("/login", new LoginHandler(&Manager));
-    server.get("/up", new ShowPage("static/upload_form.html"));
-    server.post("/up", new UploadHandler());
+    server.get("/up", new ShowPage("static/signupcss.html"));
+    server.post("/up", new SignupHandler());
     server.get("/rand", new RandomNumberHandler());
     server.get("/home.png", new ShowImage("static/home.png"));
     server.get("/", new ShowPage("static/home.html"));
